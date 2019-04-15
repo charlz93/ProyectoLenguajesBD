b32f31513df4f7d081bb573858547e04a2a026e3
carmo <car.mor0612@gmail.com>
2019-04-15 10:43:26 -0600

    Clase conexion y consulta desde codigo a la tabla dual.

---
+++ src/proyectolbd/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectolbd;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conexion con = new Conexion();
+        
+        try {
+            Statement stmt = con.conexion().createStatement();
+            ResultSet rset = stmt.executeQuery("Select (10-20) from dual");
+            
+            while(rset.next()){
+                System.out.println(rset.getInt(1));
+                
+            }
+            stmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
