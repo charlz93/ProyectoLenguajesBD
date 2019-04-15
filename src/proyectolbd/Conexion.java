dce2c3685d9d2b5cdcb1f511fe3ee1a6ef3537e7
Unknown <car.mor0612@gmail.com>
2019-04-15 13:32:56 -0600

    Conexion

---
+++ src/proyectolbd/Conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectolbd;
+import java.sql.*;
+import oracle.jdbc.*;
+/**
+ *
+ * @author carmo
+ */
+public class Conexion {
+    Connection conec;
+
+    Conexion(Connection conec) {
+        this.conec = conec;
+    }
+
+    Conexion() {
+        
+    }
+    public Connection conexion(){
+        try{
+    conec = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","proyecto","proyecto");
+   
+    }catch(Exception e){
+            System.err.println("No hay conexion error: " + e);
+    }
+         return conec;
+    }
+}    
